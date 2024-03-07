98fb6dc9e404904b7202c2cdc06068b207166596
aschahal <aschahal1234@gmail.com>
2024-03-07 00:46:41 -0600

    added style changes to second page

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -10,7 +10,46 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
+    Widget bubble = Container(
+      width: 200,
+      height: 200,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.blue
+      ),
+      child: Text(
+        'Game Bubble',
+        style: TextStyle(color: Colors.amber)
+      )
+    );
+
+    Widget dragBox = Container(
+      width: 1000,
+      height: 200,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.purple
+      ),
+      child: Text(
+        'Word Bubble, where words will be clicked and dragged',
+        textAlign: TextAlign.center,
+        style: TextStyle(color: Colors.amber)
+      )
+    );
     return Scaffold(
-        body: Center(child: Text("Game Page.. Where the magic happens")));
+        body: Center(child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            bubble,
+            SizedBox(height: 20),
+            // Align(alignment: Alignment.bottomCenter,
+            //       child: dragBox
+            // ),
+            Padding(
+              padding: EdgeInsets.only(top: 100),
+              child: dragBox
+            )
+          ],
+        )));
   }
 }
