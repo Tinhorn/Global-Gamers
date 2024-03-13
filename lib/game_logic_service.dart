5df7965e425ed83d455b8b836f7b8c2b88e8d1bf
Fawaz Joseph <AlagidiOmokunrin@gmail.com>
2024-03-12 20:36:56 -0400

    Made the word location cover a region

--- lib/game_logic_service.dart
+++ lib/game_logic_service.dart
@@ -10,8 +10,22 @@ class GamePicture {
 }
 
 class WordLocation {
+  final Coordinate topLeft;
+  final Coordinate topRight;
+  final Coordinate bottomLeft;
+  final Coordinate bottomRight;
+
+  WordLocation({
+    required this.topLeft,
+    required this.topRight,
+    required this.bottomLeft,
+    required this.bottomRight,
+  });
+}
+
+class Coordinate {
   final double x;
   final double y;
 
-  WordLocation({required this.x, required this.y});
+  Coordinate({required this.x, required this.y});
 }
